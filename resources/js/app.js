572a7c3f80a38a16a91356c412942ea728ac58f7
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-15 10:38:23 +0100

    import vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp(ExampleComponent)
+
+app.mount('#app');
+
+
+// import Vue from 'vue';
+// import ExampleComponent from './components/ExampleComponent.vue';
+
+// new Vue({
+//     el: '#app',
+//     components: { ExampleComponent }
+// });

00d475306a3b651b9e905ec9736697ec986046fc
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-26 23:36:52 +0100

    🔧 fix issues with postcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,10 @@ require('./bootstrap');
 
 import { createApp } from 'vue';
 import router from './routes';
-import VueRouter from 'vue-router';
 
 import ExampleComponent from './components/ExampleComponent.vue';
 
 const app = createApp(ExampleComponent)
-VueRouter.use(VueRouter)
 app.use(router)
 app.mount('#app');
 

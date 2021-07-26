89c7ac45ff3d3f39b7a4d648b976341d3c53d677
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-27 00:07:06 +0100

    ♻️ change Examplecomponent to base component

---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!--  <AppHeader></AppHeader>-->
+  <div class="flex flex-col min-h-screen justify-between">
+    <app-header></app-header>
+    <main class="flex flex-grow bg-100">
+      <router-view></router-view>
+    </main>
+    <app-footer></app-footer>
+  </div>
+  <!--  <AppFooter></AppFooter>-->
+</template>
+
+<script>
+
+import AppHeader from './partials/Header';
+import AppFooter from './partials/Footer';
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+}
+</script>

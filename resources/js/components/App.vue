a8155261b71f71ccc84fcf39e29c51240c6c9792
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-18 11:26:28 +0100

    ➕ add project to db

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,7 +2,7 @@
   <!--  <AppHeader></AppHeader>-->
   <div class="flex flex-col min-h-screen justify-between">
     <app-header></app-header>
-    <main class="flex flex-grow bg-100">
+    <main class="flex flex-grow bg-100 bg-blue-100">
       <router-view></router-view>
     </main>
     <app-footer></app-footer>
--- resources/js/components/projects/Projects.vue
+++ resources/js/components/projects/Projects.vue
@@ -1,9 +1,13 @@
 <template>
   <!--  {{ projects }}-->
-  <div class="p-4 m-4 bg-blue-100 rounded flex flex-col container">
+  <div class="p-4 m-4 bg-white rounded flex flex-col container">
     <div class="flex justify-between">
       <h1 class="text-2xl text-gray-700">Projects</h1>
-      <button class="bg-red-500 rounded text-white px-3 py-2 mr-6 hover:bg-red-700">Add Project</button>
+      <button @click="showForm = true" class="bg-red-500 rounded text-white px-3 py-2 mr-6 hover:bg-red-700">Add Project</button>
+    </div>
+
+    <div class="flex justify-center" v-show="showForm">
+      <project-form @cancel-form="showForm = false" @project-added="fetchProjects"></project-form>
     </div>
 
     <div class="justify-center flex">
@@ -27,18 +31,22 @@
 
 <script>
 import ProjectItem from "./ProjectItem";
+import ProjectForm from "./ProjectForm";
 
 export default {
   components: {
-    ProjectItem
+    ProjectItem,
+    ProjectForm
   },
   data() {
     return {
-      projects: []
+      projects: [],
+      showForm: false,
     }
   },
   methods: {
     fetchProjects() {
+      this.showForm = false;
       axios.get('api/projects').then((res) => {
         console.log(res);
         this.projects = res.data.data;
